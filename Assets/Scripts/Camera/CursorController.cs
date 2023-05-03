d6485612d13449ef229543cff87d6990872150e5
Artem <Artemko.one@gmail.com>
2023-05-03 15:40:52 +0300

    Fix for cursor confine

--- Assets/Scripts/Camera/CursorController.cs
+++ Assets/Scripts/Camera/CursorController.cs
@@ -1,27 +1,10 @@
-using Player;
 using UnityEngine;
 
 namespace Camera
 {
     public class CursorController : MonoBehaviour
     {
-        private void Update()
-        {
-            if (InputService.Esc)
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
-
-            if (InputService.RMB)
-            {
-                Cursor.lockState = CursorLockMode.Confined;
-            }
-        }
-
-        private void OnEnable() =>
+        private void Start() =>
             Cursor.lockState = CursorLockMode.Confined;
-
-        private void OnDisable() =>
-            Cursor.lockState = CursorLockMode.None;
     }
 }
